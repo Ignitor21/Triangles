3297982b4d1b1e78174c1d654ef5abbec6fbb22d
George <Ignit218@yandex.ru>
2023-09-26 19:03:06 +0300

    Some geometry objects implementation

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -7,9 +7,15 @@
 
 static const double EPSILON = 10e-8;
 
+bool is_equal(double lhs, double rhs)
+{
+    return (std::abs(lhs - rhs) < EPSILON);
+}
+
 class Point
 {
 public:
+
     double x = NAN, y = NAN, z = NAN;
 
     Point(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
@@ -17,18 +23,93 @@ public:
 
     void print() const
     {
-        std::cout << "(" << x << ";" << y << ")" << "\n"; 
+        std::cout << "(" << x << ";" << y << ";" << z << ")" << "\n"; 
     }
 
-    bool is_valid() const
+    bool valid() const
     {
-        return (x == x && y == y);
+        return (std::isfinite(x) && std::isfinite(y) && std::isfinite(z));
     }
 
     bool equal(const Point &rhs) const
     {
-        assert(is_valid() && rhs.is_valid());
-        return (std::abs(x - rhs.x) < EPSILON) && (std::abs(y - rhs.y) < EPSILON);
+        assert(valid() && rhs.valid());
+        return ((std::abs(x - rhs.x) < EPSILON) && (std::abs(y - rhs.y) < EPSILON) && (std::abs(z - rhs.z) < EPSILON));
     }
 };
 
+class Vector
+{
+public:
+    double x = NAN, y = NAN, z = NAN;
+
+    Vector(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
+    Vector(const Point& point) : x(point.x), y(point.y), z(point.z) {}
+    Vector() {}
+
+    void print() const
+    {
+        std::cout << "(" << x << ";" << y << ";" << z << ")" << "\n"; 
+    }
+
+    bool valid() const
+    {
+        return (std::isfinite(x) && std::isfinite(y) && std::isfinite(z));
+    }
+
+    double scalar_mult(const Vector& vec1, const Vector& vec2) const
+    {
+        assert(vec1.valid() && vec2.valid());
+        return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z); 
+    }
+
+    static Vector vector_mult(const Vector& vec1, const Vector& vec2)
+    {
+        assert(vec1.valid() && vec2.valid());
+        Vector ans;
+        ans.x = vec1.y*vec2.z - vec1.z*vec2.y;
+        ans.y = vec1.z*vec2.x - vec1.x*vec2.z;
+        ans.z = vec1.z*vec2.y - vec1.y*vec2.x;
+        return ans;
+    }
+
+    static bool collinear(const Vector& vec1, const Vector& vec2)
+    {
+        assert(vec1.valid() && vec2.valid());
+        Vector ans = vector_mult(vec1, vec2);
+        return (is_equal(ans.x, 0) && is_equal(ans.y, 0) && is_equal(ans.z, 0));
+    }
+};
+
+class Line
+{
+public:
+    Vector direction;
+    Point point;
+};
+
+class Plane
+{
+public:
+    double A = NAN, B = NAN, C = NAN, D = NAN;
+    Vector norm;
+
+    Plane(const Point& fst, const Point& snd, const Point& thd) : 
+    A((snd.y - fst.y)*(thd.z - fst.z) - (thd.y - fst.y)*(snd.z - fst.z)), 
+    B((thd.x - fst.x)*(snd.z - fst.z) - (snd.x - fst.x)*(thd.z - fst.z)),
+    C((snd.x - fst.x)*(thd.y - fst.y) - (thd.x - fst.x)*(snd.y - fst.y)),
+    D(-A*fst.x -B*fst.y -C*fst.z),
+    norm(A, B, C) {}
+
+    bool valid() const
+    {
+        return (std::isfinite(A) && std::isfinite(B) && std::isfinite(C) && std::isfinite(D));
+    }
+
+    void print() const
+    {
+        std::cout << "A = " << A << " B = " << B << " C = " << C << " D = " << D;
+        std::cout << " Normal vector = ";
+        norm.print();
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
-    Point point {1, 2, 3};
-    point.print();
+    double a = NAN;
+    double b = 0;
+    Point point {a/b, -2, 3};
+    std::cout << point.valid() << "\n";
+    Point point1{1, 0, 1};
+    Point point2{0, 1, 1};
+    Point point3{-1, 0, 1};
+    Plane plane{point1, point2, point3};
+    plane.print();
+
+    Point point4{2, 1, 5};
+    Point point5{-6, -3, -15};
+    Vector vec1(point4);
+    Vector vec2(point5);
+    std::cout << Vector::collinear(vec1, vec2) << "\n";
+    //point.print();
     return 0;
 }
